b716e20f3234425373bba2b36e0e8de399a84ec3
romi2410 <romi2410@gmail.com>
2018-09-19 23:42:35 -0500

    area lights added

--- 3D Travel/Assets/5UDE/Interactions/Steering.cs
+++ 3D Travel/Assets/5UDE/Interactions/Steering.cs
@@ -7,6 +7,7 @@ licensing agreement, is hereby granted, provided that the above copyright notice
 paragraph and the following two paragraphs appear in all copies, modifications, and 
 distributions. 
 
+
 Contact The Office of Technology Commercialization, The University of Texas at Dallas, 
 800 W. Campbell Road (AD15), Richardson, Texas 75080-3021, (972) 883-4558, 
 otc@utdallas.edu, https://research.utdallas.edu/otc for commercial licensing opportunities.
@@ -28,25 +29,24 @@ using System.Collections;
 
 public class Steering : MonoBehaviour {
 
-	// Enumerate the states of steering
-	public enum SteeringState {
-		NotSteering,
-		SteeringForward,
-		SteeringBackward,
-        //A2- Adding two new states for steering left & right
-        SteeringLeft,
-        SteeringRight
-	};
-	
+    // Enumerate the states of steering
+    public enum SteeringState {
+        NotSteering,
+        SteeringForward,
+        SteeringBackward,
+        StrafeLeft,
+        StrafeRight
+    };
+    
     // Inspector parameters
     [Tooltip("The tracking device used to determine absolute direction for steering.")]
     public CommonTracker tracker;
 
     [Tooltip("The controller joystick used to determine relative direction (forward/backward) and speed.")]
-	public CommonAxis joystick;
+    public CommonAxis joystick;
 
-	[Tooltip("A button required to be pressed to activate steering.")]
-	public CommonButton button;  
+    [Tooltip("A button required to be pressed to activate steering.")]
+    public CommonButton button;  
 
     [Tooltip("The space that is translated by this interaction. Usually set to the physical tracking space.")]
     public CommonSpace space;
@@ -54,133 +54,223 @@ public class Steering : MonoBehaviour {
     [Tooltip("The median speed for movement expressed in meters per second.")]
     public float speed = 1.0f;
 
-	// Private interaction variables
-	private SteeringState state;
+    // Private interaction variables
+    private SteeringState state;
 
-	// Called at the end of the program initialization
-	void Start () {
+    // Called at the end of the program initialization
+    void Start () {
 
-		// Set initial steering state to not steering
-		state = SteeringState.NotSteering;
-	}
-		
+        // Set initial steering state to not steering
+        state = SteeringState.NotSteering;
+    }
+        
     // FixedUpdate is not called every graphical frame but rather every physics frame
-	void FixedUpdate () {
+    void FixedUpdate () {
 
-		// If state is not steering
-		if (state == SteeringState.NotSteering) {
-		
-			// If the joystick is pressed forward and the button is pressed
-			if (joystick.GetAxis ().y > 0.0f && button.GetPress ()) {
+        // If state is not steering
+        if (state == SteeringState.NotSteering) {
+        
+            // If the joystick is pressed forward and the button is pressed
+            if (joystick.GetAxis ().y > 0.0f && button.GetPress () ) {
 
-				// Change state to steering forward
-				state = SteeringState.SteeringForward;
-			}
-				
-			// If the joystick is pressed backward and the button is pressed
-			else if (joystick.GetAxis ().y < 0.0f && button.GetPress ()) {
+                // Change state to steering forward
+                state = SteeringState.SteeringForward;
+            }
+                
+            // If the joystick is pressed backward and the button is pressed
+            else if (joystick.GetAxis ().y < 0.0f && button.GetPress () ) {
 
-				// Change state to steering backward
-				state = SteeringState.SteeringBackward;
-			}
-
-            //A2 - If the joystick is pressed left and the button is pressed
-            else if (joystick.GetAxis().x < 0.0f && button.GetPress()) {
-                //A2 - change state to SteeringLeft
-                state = SteeringState.SteeringLeft;
+                // Change state to steering backward
+                state = SteeringState.SteeringBackward;
             }
 
-            //A2 - If the joystick is pressed right and the button is pressed
+            // If the joystick is pressed left and the button is pressed
+            else if (joystick.GetAxis().x < 0.0f && button.GetPress() )
+            {
+
+                // Change state to strafeleft
+                state = SteeringState.StrafeLeft;
+            }
+
+            // If the joystick is pressed right and the button is pressed
+            else if (joystick.GetAxis().x > 0.0f && button.GetPress() )
+            {
+
+                // Change state to strafe right
+                state = SteeringState.StrafeRight;
+            }
+
+
+
+            // Process current not steering state
+            else {
+
+                // Nothing to do for not steering
+            }
+        }
+
+        // If state is steering forward
+        else if (state == SteeringState.SteeringForward) {
+
+            // If the button is not pressed
+            if (!button.GetPress ()) {
+
+                // Change state to not steering 
+                state = SteeringState.NotSteering;
+            }
+
+            // If the joystick is pressed left and the button is pressed
+            else if (joystick.GetAxis().x < 0.0f && button.GetPress())
+            {
+
+                // Change state to strafe left
+                state = SteeringState.StrafeLeft;
+            }
+
+            // If the joystick is pressed backward and the button is pressed
+            else if (joystick.GetAxis ().y < 0.0f && button.GetPress () ) {
+
+                // Change state to steering backward
+                state = SteeringState.SteeringBackward;
+            }
+
+            // If the joystick is pressed right and the button is pressed
             else if (joystick.GetAxis().x > 0.0f && button.GetPress())
             {
-                //A2 - change state to SteeringRight
-                state = SteeringState.SteeringRight;
+
+                // Change state to strafe right
+                state = SteeringState.StrafeRight;
             }
 
-			// Process current not steering state
-			else {
+            // Process current steering forward state
+            else {
 
-				// Nothing to do for not steering
-			}
-		}
+                // Translate the space based on the tracker's absolute forward direction and the joystick's forward value
+                space.transform.position += joystick.GetAxis ().y * tracker.transform.forward * speed * Time.deltaTime;
+            }
+        }
 
-		// If state is steering forward
-		else if (state == SteeringState.SteeringForward) {
+        // If state is steering backward
+        else if (state == SteeringState.SteeringBackward) {
 
-			// If the button is not pressed
-			if (!button.GetPress ()) {
+            // If the button is not pressed
+            if (!button.GetPress ()) {
 
-				// Change state to not steering 
-				state = SteeringState.NotSteering;
-			}
+                // Change state to not steering 
+                state = SteeringState.NotSteering;
+            }
 
-            //A2 - If the joystick is pressed left and the button is pressed
-            else if (joystick.GetAxis().y < 0.0f && button.GetPress())
+            // If the joystick is pressed left and the button is pressed
+            else if (joystick.GetAxis().x < 0.0f && button.GetPress() )
             {
 
-                //A2 - Change state to steering left
-                state = SteeringState.SteeringLeft;
+                // Change state to strafe left
+                state = SteeringState.StrafeLeft;
             }
 
-            //A2 - If the joystick is pressed right and the button is pressed
+            // If the joystick is pressed forward and the button is pressed
+            else if (joystick.GetAxis ().y > 0.0f && button.GetPress () ) {
+
+                // Change state to steering forward
+                state = SteeringState.SteeringForward;
+            }
+
+            // Process current steering backward state
+            else {
+
+                // Translate the space based on the tracker's absolute forward direction and the joystick's backward value
+                space.transform.position += joystick.GetAxis ().y * tracker.transform.forward * speed * Time.deltaTime;
+            }
+        }
+
+        //if state is strafing left
+        else if (state == SteeringState.StrafeLeft)
+        {
+
+            // If the button is not pressed
+            if (!button.GetPress())
+            {
+
+                // Change state to not steering 
+                state = SteeringState.NotSteering;
+            }
+
+            // If the joystick is pressed forward and the button is pressed
+            else if (joystick.GetAxis().y > 0.0f && button.GetPress() )
+            {
+
+                // Change state to steering forward
+                state = SteeringState.SteeringForward;
+            }
+
+            // If the joystick is pressed backward and the button is pressed
+            else if (joystick.GetAxis().y < 0.0f && button.GetPress() )
+            {
+
+                // Change state to steering backward
+                state = SteeringState.SteeringBackward;
+            }
+
+            // If the joystick is pressed right and the button is pressed
+            else if (joystick.GetAxis().x > 0.0f && button.GetPress() )
+            {
+
+                // Change state to strafe right
+                state = SteeringState.StrafeRight;
+            }
+
+            // Process current strafe left state
+            else
+            {
+
+                // Translate the space based on the tracker's absolute forward direction and the joystick's backward value
+                space.transform.position += joystick.GetAxis().x * tracker.transform.right * speed * Time.deltaTime;
+            }
+        }
+
+        //if state is strafing right
+        else if (state == SteeringState.StrafeRight)
+        {
+
+            // If the button is not pressed
+            if (!button.GetPress())
+            {
+
+                // Change state to not steering 
+                state = SteeringState.NotSteering;
+            }
+
+            // If the joystick is pressed forward and the button is pressed
             else if (joystick.GetAxis().y > 0.0f && button.GetPress())
             {
 
-                //A2 - Change state to steering left
-                state = SteeringState.SteeringLeft;
+                // Change state to steering forward
+                state = SteeringState.SteeringForward;
             }
 
-			// If the joystick is pressed backward and the button is pressed
-			else if (joystick.GetAxis ().y < 0.0f && button.GetPress ()) {
-
-				// Change state to steering backward
-				state = SteeringState.SteeringBackward;
-			}
-
-			// Process current steering forward state
-			else {
-
-				// Translate the space based on the tracker's absolute forward direction and the joystick's forward value
-				space.transform.position += joystick.GetAxis ().y * tracker.transform.forward * speed * Time.deltaTime;
-			}
-		}
-
-		// If state is steering backward
-		else if (state == SteeringState.SteeringBackward) {
-
-			// If the button is not pressed
-			if (!button.GetPress ()) {
-
-				// Change state to not steering 
-				state = SteeringState.NotSteering;
-			}
-
-            //A2 - If the joystick is pressed left and the button is pressed
-            else if (joystick.GetAxis().x < 0.0f && button.GetPress()) {
-                //A2 - Change state to steering left
-                state = SteeringState.SteeringLeft;
-            }
-
-            //A2 - If the joystick is pressed right and the button is pressed
-            else if (joystick.GetAxis().x > 0.0f && button.GetPress())
+            // If the joystick is pressed backward and the button is pressed
+            else if (joystick.GetAxis().y < 0.0f && button.GetPress() )
             {
-                //A2 - Change state to steering right
-                state = SteeringState.SteeringRight;
+
+                // Change state to steering backward
+                state = SteeringState.SteeringBackward;
             }
 
-			// If the joystick is pressed forward and the button is pressed
-			else if (joystick.GetAxis ().y > 0.0f && button.GetPress ()) {
+            // If the joystick is pressed left and the button is pressed
+            else if (joystick.GetAxis().x < 0.0f && button.GetPress())
+            {
 
-				// Change state to steering forward
-				state = SteeringState.SteeringForward;
-			}
+                // Change state to strafing left
+                state = SteeringState.StrafeLeft;
+            }
 
-			// Process current steering backward state
-			else {
+            // Process current strafe right state
+            else
+            {
 
-				// Translate the space based on the tracker's absolute forward direction and the joystick's backward value
-				space.transform.position += joystick.GetAxis ().y * tracker.transform.forward * speed * Time.deltaTime;
-			}
-		}
-	}
+                // Translate the space based on the tracker's absolute forward direction and the joystick's backward value
+                space.transform.position += joystick.GetAxis().x * tracker.transform.right * speed * Time.deltaTime;
+            }
+        }
+    }
 }
